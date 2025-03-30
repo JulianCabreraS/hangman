2b80da929c773209af2d89c178f4ef2d9a0060b8
Julian Cabrera <julian.cabreras@gmail.com>
2025-03-30 15:35:09 -0600

    Dictionary completion

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,5 +1,9 @@
 defmodule Dictionary do
-  def hello do
-    IO.puts("Hello, World!!!!")
+  @word_list "assets/words.txt"
+             |> File.read!()
+             |> String.split(~r/\n/, trim: true)
+
+  def random_word do
+    @word_list |> Enum.random()
   end
 end
