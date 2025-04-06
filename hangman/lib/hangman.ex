5f14ef5c45f41a9f44f0f857214f8ff0f66945a7
Julian Cabrera <julian.cabreras@gmail.com>
2025-04-06 15:10:37 -0600

    Refactor Hangman module: delegate new_game to Game implementation, remove unused file, and add dictionary dependency. Include initial test for Hangman module.

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,6 +1,8 @@
 defmodule Hangman do
+  alias Hangman.Impl.Game
+
   @type state :: :initializing | :won | :lost | :good_guess | :bad_guess | :already_used
-  @type game :: any
+  @opaque game :: Game.t()
   @type tally :: %{
           turns_left: integer,
           game_state: state,
@@ -8,11 +10,11 @@ defmodule Hangman do
           used: list(String.t())
         }
 
-  @spec new_game :: game
-  def new_game do
-  end
+  @spec new_game() :: game
+  defdelegate new_game(), to: Game
 
   @spec make_move(game, String.t()) :: {game, tally}
-  def make_move(game, guess) do
+  def make_move(_game, _guess) do
+    raise "Not implemented"
   end
 end
